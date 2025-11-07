d7406a52c56d1c46b8057ff3fa5150f372964d87
kayakku06 <yoto6088@google.com>
2025-11-07 15:02:03 +0900

    setup: lucideiconを使えるように構築

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,13 @@
+<script setup>
+// このページで使うアイコンだけを直接インポート
+import { AlignHorizontalJustifyEnd, User, Settings } from 'lucide-vue-next';
+</script>
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">
-  </h1>
-    
-    <NuxtWelcome />
-   
+    </h1>
+    <AlignHorizontalJustifyEnd />
+    <User />
+    <Settings />
   </div>
 </template>
