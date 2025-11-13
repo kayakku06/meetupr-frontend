685a28ec245fe8b979c4c3a3164cc427ee17d97a
yui <yuixicun97@gmail.com>
2025-11-13 17:28:45 +0900

    ユーザーアイコンのコンポーネントの作成

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,14 +1,29 @@
-<template>
-    <div>
-        <h1>Home 11</h1>
-        
-    </div>
-</template>
-
 <script setup>
-// 必要ならここにロジックを書く
+import ChatIcon from '~/components/ChatIcon.vue'
 </script>
 
+<!-- public/index.html の <head> の中に追加 -->
+
+<template>
+  <div class="p-4">
+    <h1 class="text-xl mb-4 zen-maru-gothic-regular">Home 11</h1>
+
+    <!-- ChatIcon コンポーネントを呼び出し -->
+    <ChatIcon 
+      name="ユーザー１"
+      message="メッセージ"
+      date="11/3"
+      avatarColor="bg-teal-600"
+    />
+    <ChatIcon 
+      name="ユーザー２"
+      message="こんにちは！"
+      date="11/10"
+      avatarColor="bg-teal-600"
+    />
+  </div>
+</template>
+
 <style scoped>
-/* スタイルを追加する場合 */
+/* 必要ならスタイルをここに追加 */
 </style>
