b108ca5b6423985e86536aff923181f494528c73
kayakku06 <yoto6088@google.com>
2025-11-15 04:56:21 +0900

    チャット画面の作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,6 @@ export default {
     './pages/**/*.{vue,js,ts,jsx,tsx}',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#4a90e2',
-        selected: '#5a9a94',
-        warning: '#e6a23c',
-        basebg: '#FFF5C9',
-        container: '#FEBC6E',
-        tagbg: '#fceb96',
-      },
-      borderWidth: {
-        3: '3px',
-      },
-      maxWidth: {
-        '420': '420px',
-      },
-    },
-  },
+  
   plugins: [],
 }
