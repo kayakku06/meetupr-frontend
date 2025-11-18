528b7d323a1f405398c67df48dfc8dc14b55b0e1
yui <yuixicun97@gmail.com>
2025-11-18 21:05:41 +0900

    フッターの編集

--- app/pages/other-profile.vue
+++ app/pages/other-profile.vue
@@ -10,10 +10,16 @@
 
         <div class="flex-1">
           <h2 class="m-0 mb-2 text-lg text-[#2f5f56]">{{ user.name }}</h2>
-          <button @click="sendMessage" class="inline-flex items-center gap-2 px-3 py-1.5 rounded-full text-sm text-white" :class="['bg-[var(--meetupr-color-3)]']">
-            <svg width="14" height="14" viewBox="0 0 24 24" aria-hidden><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm4 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h12v14z" fill="#fff"/></svg>
+          <button 
+            class="border-[var(--meetupr-color-3)] text-white px-2.5 py-1.5 rounded-full inline-flex gap-1.5 items-center text-xs hover:border-[var(--meetupr-color-3)] transition bg-[var(--meetupr-color-3)]" 
+            @click="goMessage"
+>           <svg width="14" height="14" viewBox="0 0 24 24" aria-hidden>
+              <path d="M21 6.5a2.5 2.5 0 0 0-2.5-2.5h-13A2.5 2.5 0 0 0 3 6.5v13l4-4h11.5A2.5 2.5 0 0 0 21 13V6.5z" 
+              fill="#fff"/>
+            </svg>
             メッセージ
           </button>
+
         </div>
       </div>
 
@@ -58,23 +64,17 @@
         </label>
       </div>
     </main>
+       <Footer class="fixed inset-x-0 bottom-0" />
 
-    <nav class="fixed left-0 right-0 bottom-0 h-14 flex justify-around items-center shadow-[0_-2px_6px_rgba(0,0,0,0.08)]" :class="['bg-[var(--meetupr-sub)]']" aria-hidden>
-      <button class="w-14 h-14 flex items-center justify-center rounded-full text-white">
-        <svg width="22" height="22" viewBox="0 0 24 24"><path d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z" fill="#fff"/></svg>
-      </button>
-      <button class="w-14 h-14 flex items-center justify-center rounded-full text-white">
-        <svg width="22" height="22" viewBox="0 0 24 24"><path d="M21 6.5a2.5 2.5 0 0 1-2.5 2.5H8l-5 5V4A2 2 0 0 1 5 2h14.5A2.5 2.5 0 0 1 21 6.5z" fill="#fff"/></svg>
-      </button>
-      <button class="w-14 h-14 flex items-center justify-center rounded-full text-white">
-        <svg width="22" height="22" viewBox="0 0 24 24"><path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10zm0 2c-4.4 0-8 2.2-8 4.9V22h16v-3.1c0-2.7-3.6-4.9-8-4.9z" fill="#fff"/></svg>
-      </button>
-    </nav>
+   
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import Footer from '~/components/Footer.vue'
+import { MessageCircle } from 'lucide-vue-next'
 
 const user = ref({
   name: 'ユーザー1',
@@ -86,8 +86,10 @@ const user = ref({
   bio: 'よろしくお願いします！！'
 })
 
+const router = useRouter()
 function sendMessage() {
   alert(`${user.value.name} さんにメッセージを送信しますか？`)
   // 実際はメッセージ画面に遷移
 }
+
 </script>
